62a54f291a988acaa7581a5151e999418aaec8eb
angelique-df <faye.angelique.d@gmail.com>
2022-08-09 21:09:15 +0000

    Create Header.js

---
+++ pages/Header.js
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+import CallToActionButton from "./CallToActionButton";
+
+import Logo from "./../public/images/logo.svg";
+import MenuButton from "./../public/images/icon-hamburger.svg";
+
+function Header({ linkedPages }) {
+	return (
+		<header>
+			<h1>
+				<a href="#">
+					<Image src={Logo} alt="Manage"></Image>
+				</a>
+			</h1>
+			<button
+				style={{ backgroundImage: `url(${MenuButton})` }}
+				className="toggle-mobile-menu"
+				title="Open mobile menu"
+			></button>
+			<nav>
+				{linkedPages.map((page, index) => (
+					<a key={page + "-" + index} href="#">
+						{page}
+					</a>
+				))}
+			</nav>
+
+			<CallToActionButton />
+		</header>
+	);
+}
+
+export default Header;
