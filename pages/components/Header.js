31c38ba46a6ecf70fbdaa3f883a7ab05c417c54f
angelique-df <faye.angelique.d@gmail.com>
2022-08-10 21:12:55 +0000

    Update `Header` component
    - Add state to track `mobileMenu`
    - Add mobile menu button click handler

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import Image from "next/image";
 
 import CallToActionButton from "./CallToActionButton";
@@ -6,6 +8,26 @@ import Logo from "./../../public/images/logo.svg";
 import MenuButton from "./../../public/images/icon-hamburger.svg";
 
 function Header({ linkedPages }) {
+	// Fix reference error, document is undefined
+	const [_document, set_document] = useState(null);
+	useEffect(() => {
+		set_document(document);
+	}, []);
+
+	/**
+	 * Tracks whether the mobile menu is open
+	 */
+	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+	function handleToggleBtnClick() {
+		// toggle ".mobile-menu-open" on body
+		_document.body.classList.toggle("mobile-menu-open");
+
+		setMobileMenuOpen((prev) => {
+			// toggle mobileMenuOpen to conditionally add the same class to "nav" and set the path for the toggler button's background image
+			return !prev;
+		});
+	}
+
 	return (
 		<header>
 			<h1>
