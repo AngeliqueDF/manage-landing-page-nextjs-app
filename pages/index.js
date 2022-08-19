dc35047d497c9e907c050d318c69625f096434ff
angelique-df <faye.angelique.d@gmail.com>
2022-08-19 19:11:18 +0000

    Add `featuresSection` data

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,31 @@ export default function Home() {
 			copyText:
 				"Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goals in view.",
 		},
+		featuresSection: {
+			heading: "What’s different about Manage?",
+			copyText:
+				"Manage provides all the functionality your team needs, without the complexity. Our software is tailor-made for modern digital product teams.",
+			featuresList: [
+				{
+					id: 0,
+					heading: "Track company-wide progress",
+					copyText:
+						"See how your day-to-day tasks fit into the wider vision. Go from tracking progress at the milestone level all the way done to the smallest of details. Never lose sight of the bigger picture again.",
+				},
+				{
+					id: 1,
+					heading: "Advanced built-in reports",
+					copyText:
+						"Set internal delivery estimates and track progress toward company goals. Our customisable dashboard helps you build out the reports you need to keep key stakeholders informed.",
+				},
+				{
+					id: 2,
+					heading: "Everything you need in one place",
+					copyText:
+						"Stop jumping from one service to another to communicate, store files, track tasks and share documents. Manage offers an all-in-one team productivity solution.",
+				},
+			],
+		},
 	};
 	return (
 		<div className={styles.container}>
