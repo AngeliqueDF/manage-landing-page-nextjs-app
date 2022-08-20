774ae6ec63e6f18278d4681a8be0346ddab7c8fe
angelique-df <faye.angelique.d@gmail.com>
2022-08-20 16:50:46 +0000

    Import testimonials avatars

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import Head from "next/head";
-import Image from "next/image";
+
 import styles from "../styles/Home.module.css";
 
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Features from "../components/Features";
+
+import Testimonials from "../components/Testimonials/Testimonials";
+// Profile pictures for slider
+import AnishaLiPicture from "./../public/images/avatar-anisha.png";
+import AliBravoPicture from "./../public/images/avatar-ali.png";
+import RichardWattsPicture from "./../public/images/avatar-richard.png";
+import ShanaiGoughPicture from "./../public/images/avatar-shanai.png";
 
 export default function Home() {
 	const data = {
