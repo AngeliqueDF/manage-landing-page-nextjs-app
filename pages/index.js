13d8cd622c7c08a38150b2aa7ec1595227f85c3a
angelique-df <faye.angelique.d@gmail.com>
2022-08-20 15:58:30 +0000

    Add `testimonialsSection` data

--- pages/index.js
+++ pages/index.js
@@ -43,6 +43,47 @@ export default function Home() {
 				},
 			],
 		},
+		testimonialsSection: {
+			heading: "What they’ve said",
+			reviews: [
+				{
+					id: 0,
+					author: {
+						id: 0,
+						name: "Anisha Li",
+						profilePicture: AnishaLiPicture,
+					},
+					text: "“Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.”",
+				},
+				{
+					id: 1,
+					author: {
+						id: 1,
+						name: "Ali Bravo",
+						profilePicture: AliBravoPicture,
+					},
+					text: "“We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.”",
+				},
+				{
+					id: 2,
+					author: {
+						id: 2,
+						name: "Richard Watts",
+						profilePicture: RichardWattsPicture,
+					},
+					text: "“Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I tallk to!”",
+				},
+				{
+					id: 3,
+					author: {
+						id: 3,
+						name: "Shanai Gough",
+						profilePicture: ShanaiGoughPicture,
+					},
+					text: "“Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.”",
+				},
+			],
+		},
 	};
 	return (
 		<div className={styles.container}>
