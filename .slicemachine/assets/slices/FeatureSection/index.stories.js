80535da7c297cf2b4428fc2e30380529e3424c46
angelique-df <faye.angelique.d@gmail.com>
2022-09-18 20:40:57 +0000

    Install `prismic`

---
+++ .slicemachine/assets/slices/FeatureSection/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/FeatureSection';
+
+export default {
+  title: 'slices/FeatureSection'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Although","spans":[]}],"description":[{"type":"paragraph","text":"Nulla nulla ullamco cupidatat sint.","spans":[]}]},"slice_type":"feature_section","id":"_Default"}} />
+_Default.storyName = ''
---
+++ .slicemachine/assets/slices/LinkedPages/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/LinkedPages';
+
+export default {
+  title: 'slices/LinkedPages'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"link_text_content":[{"type":"paragraph","text":"Ex et in ea veniam.","spans":[]}],"link_url":{"link_type":"Web","url":"http://twitter.com"}}],"primary":{},"slice_type":"linked_pages","id":"_Default"}} />
+_Default.storyName = ''
